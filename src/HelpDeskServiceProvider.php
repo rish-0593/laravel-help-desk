69fbf989f55ee5238646bb30853ec9e623f2277b
Rishabh Kumar <kumarrishabh050@gmail.com>
2023-04-10 19:32:09 +0530

    initial commit

---
+++ src/HelpDeskServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Rish0593\HelpDesk;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelpDeskServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
